a7f4b013be354c7f39ce293047c7dad96db813d9
Bohdan Samotys <=>
2021-01-31 17:57:41 +0100

    create seeds, controllers, routes and header

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  root "transactions#index"
-  get 'transactions/index'
-  get 'transactions/new'
-  get 'transactions/edit'
+  root "welcome#index"
+  resources :transactions
+  resources :accounts
+  resources :analytics, only: :index do
+    collection do
+      resources :categories, except: :index
+      resources :areas, except: :index
+    end
+  end
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+zloty = Currency.create({ name: 'Zloty', code: 'PLN', number: 985 })
+dollar = Currency.create({ name: 'US Dollar', code: 'USD', number: 840 })
+euro = Currency.create({ name: 'Euro', code: 'EUR', number: 978 })
+hryvnia = Currency.create({ name: 'Hryvnia', code: 'UAH', number: 980 })
+
+CurrencyRate.create([
+                      { from: dollar, to: euro, rate: 0.8155, source: 'Revolut' },
+                      { from: euro, to: dollar, rate: 1.2007, source: 'Revolut' },
+                      { from: zloty, to: dollar, rate: 0.2654, source: 'Revolut' },
+                      { from: zloty, to: euro, rate: 0.2186, source: 'Revolut' },
+                      { from: dollar, to: zloty, rate: 3.6842, source: 'Revolut' },
+                      { from: euro, to: zloty, rate: 4.4682, source: 'Revolut' },
+                      { from: hryvnia, to: dollar, rate: 0.0354, source: 'monobank'},
+                      { from: hryvnia, to: euro, rate: 0.0292, source: 'monobank'},
+                      { from: hryvnia, to: zloty, rate: 0.1317, source: 'monobank'},
+                      { from: dollar, to: hryvnia, rate: 28.0, source: 'monobank'},
+                      { from: euro, to: hryvnia, rate: 33.8, source: 'monobank'},
+                      { from: zloty, to: hryvnia, rate: 7.41, source: 'monobank'}
+                    ])
