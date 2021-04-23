30960ca5d22cfc334927392b9c0051c40955ce69
Hlib Hraif <hlib.hraif@protonmail.com>
2021-04-23 12:51:52 +0300

    Fix a bug

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,4 +1,4 @@
 class Loan < ApplicationRecord
   belongs_to :user
-  has_and_belongs_to_many :transactions
+  has_and_belongs_to_many :exchanges, class_name: 'Transaction'
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,7 +3,7 @@ class Transaction < ApplicationRecord
   belongs_to :area
   belongs_to :account
   belongs_to :destination_account, class_name: 'Account', foreign_key: 'destination_account_id', optional: true
-  has_and_belongs_to_many :loans
+  has_one :loan, inverse_of: :exchanges
 
   after_create :alter_account_amount
 
