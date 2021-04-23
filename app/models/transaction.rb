513b309d7c70517243bf3f3902009180e3018fa9
Hlib Hraif <hlib.hraif@protonmail.com>
2021-04-23 12:41:34 +0300

    Fix a bug

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,7 +3,7 @@ class Transaction < ApplicationRecord
   belongs_to :area
   belongs_to :account
   belongs_to :destination_account, class_name: 'Account', foreign_key: 'destination_account_id', optional: true
-  has_one :loan
+  has_and_belongs_to_many :loans
 
   after_create :alter_account_amount
 
