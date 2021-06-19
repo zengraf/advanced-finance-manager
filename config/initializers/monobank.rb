ef6aa440324281d130096060fa511ce304a75a05
Hlib Hraif <hlib.hraif@protonmail.com>
2021-06-19 15:32:17 +0200

    Remove monobank initializer

--- config/initializers/monobank.rb
+++
@@ -1,9 +0,0 @@
-require "monobank_api"
-
-Rails.configuration.after_initialize do
-  if defined?(Rails::Server)
-    User.where.not(monobank_token: nil).find_each do |user|
-      MonobankApi.webhook(user)
-    end
-  end
-end
